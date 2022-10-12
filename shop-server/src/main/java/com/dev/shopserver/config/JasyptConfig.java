79dd02717a90a92a006d53a9cc990d547a9de20f
YHawon <dbsgk1102@gmail.com>
2022-10-12 21:00:56 +0900

    jasypt를 이용한 DB 접속정보 암호화 추가

---
+++ shop-server/src/main/java/com/dev/shopserver/config/JasyptConfig.java
@@ -0,0 +1,24 @@
+package com.dev.shopserver.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPassword("dev_Key"); // 암호화 키 값
+        config.setPoolSize("1");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
+
